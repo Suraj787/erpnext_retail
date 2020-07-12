46e402a2c52a3959ed24b8956d65035bfaf83930
Palash Jhabak <palash@techlift.in>
2020-07-12 12:45:09 +0530

    Revert "Price based tax rule"

--- erpnext_retail/hooks.py
+++ erpnext_retail/hooks.py
@@ -29,8 +29,7 @@ page_js = {"point-of-sale": "public/js/point_of_sale.js"}
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 doctype_js = {
-        "Purchase Invoice": "public/js/purchase_invoice.js",
-        "Sales Invoice": "public/js/sales_invoice.js"
+	"Purchase Invoice": "public/js/purchase_invoice.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -83,6 +82,7 @@ doctype_js = {
 # Document Events
 # ---------------
 # Hook on document methods and events
+
 # doc_events = {
 # 	"*": {
 # 		"on_update": "method",
@@ -130,3 +130,4 @@ doctype_js = {
 # override_doctype_dashboards = {
 # 	"Task": "erpnext_retail.task.get_dashboard_data"
 # }
+
