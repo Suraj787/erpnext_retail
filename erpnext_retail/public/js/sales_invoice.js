20b0bdb75371eb0eba70e79291ae4a432fdcdd1e
bizmapNikhil <nikhil.tulankar@bizmap.in>
2022-09-20 18:25:20 +0530

    [add][payment request on sales invoice]

---
+++ erpnext_retail/public/js/sales_invoice.js
@@ -0,0 +1,23 @@
+frappe.ui.form.on('Sales Invoice', {
+	refresh:function(frm){
+        frm.add_custom_button(__('Payment Request'), function() {
+            frappe.call({
+				method: "erpnext_retail.erpnext_retail.custom_script.sales_invoice.make_payment_request",
+				args:{
+		          'doc':frm.doc
+		        },
+		        callback: function(r) {
+					if(r.message) {
+						frappe.msgprint({
+							message: __('Payment Request Created: {0}',
+									 [repl('<a href="/app/payment-request/%(name)s">%(name)s</a>', {name:r.message})]
+								),
+							indicator: 'green'
+						})
+					}
+				}
+			});
+		})
+	}
+});
+	                
