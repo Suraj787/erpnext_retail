24e209a6b90013c256126e8d4c127f8aba2e79c1
bizmapNikhil <nikhil.tulankar@bizmap.in>
2022-09-22 11:45:00 +0530

    [fix][email as contact person on payment request]

--- erpnext_retail/erpnext_retail/custom_script/sales_invoice.py
+++ erpnext_retail/erpnext_retail/custom_script/sales_invoice.py
@@ -22,12 +22,12 @@ def make_payment_request(doc):
         pr_doc.payment_gateway = gateway_account.get("payment_gateway")
         pr_doc.payment_account = gateway_account.get("payment_account")
         pr_doc.payment_channel = gateway_account.get("payment_channel")
-        pr_doc.email_to = doc.get('owner')
+        pr_doc.email_to = get_email_id(doc)
         pr_doc.subject = _("Payment Request for {0}").format(doc.get('name'))
         pr_doc.message = gateway_account.get("message") 
         pr_doc.save()
         pr_doc.submit()
-        
+
         return pr_doc.name
     else:
         frappe.msgprint("Payment Request already exist.")
@@ -42,6 +42,16 @@ def check_duplicate_payment_request(doc):
             'docstatus':1,
             'name': ['!=', doc.get('name')]
         }, limit=1)
+
+def get_email_id(doc):
+    if doc.get('contact_email'):
+        return doc.get('contact_email')
+    elif doc.get('contact_person'):
+        email = frappe.db.get_value("Contact", {'name':doc.get('contact_person')}, 'email_id')
+        if email:
+            return email
+    else:
+        return doc.get('owner')
 
 def get_grand_total(doc):
     if doc.get('party_account_currency') == doc.get('currency'):
