13d52658344a823bc18a228f327f42b630c03d0c
hidayatbizmap <107534037+hidayatbizmap@users.noreply.github.com>
2022-11-28 19:17:24 +0530

    Update payment_request.py

--- erpnext_retail/erpnext_retail/custom_script/payment_request.py
+++ erpnext_retail/erpnext_retail/custom_script/payment_request.py
@@ -8,3 +8,28 @@ def on_submit(doc,method):
         frappe.db.sql("""update `tabSales Invoice` set payment_request_url = '{0}'
             where name = '{1}'""".format(doc.payment_url,doc.reference_name))
         frappe.db.commit()
+ 
+@frappe.whitelist()
+def auto_sales_invoice(so):
+    sales_order = frappe.get_doc("Sales Order",so)
+    si = frappe.new_doc("Sales Invoice")
+    si.customer = sales_order.customer
+    si.posting_date = sales_order.transaction_date
+    for i in sales_order.items:
+        
+        si.append("items",{
+                'item_code':i.item_code,
+                'item_name':i.item_name,
+                'description':i.description,
+                'uom':i.uom,
+                'qty':i.qty,
+                'conversion_factor':"1",
+                'warehouse':i.warehouse,
+                'rate':i.rate,
+                'amount':i.amount
+
+                                                
+                })
+    si.insert() 
+    si.submit() 
+    frappe.msgprint("Create Sales Invoice")
